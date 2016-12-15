6afb6d38a5b334a5d96d5346c1a902765e8b9986
Michael Pevzner <mihapza@gmail.com>
2016-12-15 17:12:07 +0200

    links added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router';
 import Classes from './styles';
+
+const links = [
+  { to: '/', children: 'Home' },
+  { to: '/dashboard', children: 'Dashboard' }
+];
 
 const Header = () => (
   <div className={ Classes.root }>
-    sqdafasdf
+    { links.map((props, i) =>
+      <Link
+        key={ i }
+        activeClassName={ Classes.active }
+        activeOnlyWhenExact
+        { ...props }
+      />
+    )}
   </div>
 );
 
