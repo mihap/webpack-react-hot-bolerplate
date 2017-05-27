45e10a8c40d0f0fe1eeb02d4feab9d0e07c44d35
Michael Pevzner <mihapza@gmail.com>
2017-05-27 16:24:25 +0300

    clean

--- src/components/AsyncComponent/index.js
+++ src/components/AsyncComponent/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 const asyncComponent = (getComponent) =>
   class AsyncComponent extends React.Component {
-    static Component = null;
     state = { Component: AsyncComponent.Component };
 
     componentWillMount() {
       if (!this.state.Component) {
         getComponent().then(Component => {
-          AsyncComponent.Component = Component;
           this.setState({ Component });
         });
       }
